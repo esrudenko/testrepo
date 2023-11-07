14e4335867bbda6e572949621c9a7c0c9f5424c4
Elena Mankerova <esrudenko93@gmail.com>
2023-11-07 10:19:23 +0600

    init: add cv.md file with information (resume) in it

--- HelloNode/index.js
+++ HelloNode/index.js
@@ -1,1 +1,33 @@
-console.log((8 / 2 + 5 - -3 / 2))
+ccn = 371449635398431;
+const ccnStr = String(ccn);
+const arrCcn = [];
+
+for (let i = 0; i < ccnStr.length; i += 1) {
+    if (i % 2 !== 0) {
+        arrCcn.push(Number(ccnStr[i]));
+    } else { arrCcn.push(Number(ccnStr[i] * 2)); }
+}
+console.log(arrCcn);
+/*for (let i = 0; i < arrCcn.length; i += 1) {
+    if (arrCcn[i] > 9) {
+        arrCcn[i] = arrCcn[i] - 9;
+    }
+}
+console.log(arrCcn);
+const sumCCN = (arrCcn.reduce((sum, current) => sum + current));
+console.log(sumCCN);
+if ((sumCCN % 10) === 0) {
+    console.log(true);
+} else { console.log(false) };
+*/
+const arr = ccn.toString().split('').map((elem) => Number(elem));
+
+for (let i = arr.length - 2; i >= 0; i -= 2) {
+    if ((arr[i] * 2) > 9) {
+        arr[i] = (arr[i] * 2) - 9;
+    } else {
+        arr[i] *= 2;
+    }
+}
+const sumCCN = (arrCcn.reduce((sum, current) => sum + current))
+return (sumCCN % 10) === 0;
--- tasks/index.js
+++ tasks/index.js
@@ -1,13 +1,19 @@
-function getDecimal(n){
-    let decimalToPart;
-    let decimalPart;
-    decimalToPart = (n + '').split('.');
-    if (decimalToPart.length === 1) {
-      return 0;
-    } else {
-      decimalPart = '0.' + decimalToPart[1];
-    }
-    return Number(decimalPart);
+function getDecimal(n) {
+  let decimalToPart;
+  let decimalPart;
+  decimalToPart = (n + '').split('.');
+  if (decimalToPart.length === 1) {
+    return 0;
+  } else {
+    decimalPart = '0.' + decimalToPart[1];
   }
-  getDecimal(-2.5)
-  console.log(getDecimal)
+  return Number(decimalPart);
+}
+getDecimal(-2.5)
+console.log(getDecimal)
+
+
+let admin;
+let name = 'John';
+name = admin;
+alert(admin);
